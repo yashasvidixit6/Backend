a2b3f9fa9e24d553e01d0e833a1916b88f68d866
yashasvidixit6 <yashasvidixit6@gmail.com>
2025-05-26 14:43:30 +0530

    awx

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 const url="mongodb+srv://yashasvidixit6:NeW11a14vvHPvCP9@cluster1.wtzoxk8.mongodb.net/tenderdb?retryWrites=true&w=majority&appName=Cluster1";
 mongoose.connect(url,{
+    useNewUrlParser:true,
+    useUnifiedTopolgy:true,
     serverSelectionTimeoutMS:5000
 });
 
