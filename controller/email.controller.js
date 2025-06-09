b9e425bdbeb1211f3fcfbd6a5e7d86849e9a896c
yashasvidixit6 <yashasvidixit6@gmail.com>
2025-06-09 12:09:16 +0530

    url added

--- controller/email.controller.js
+++ controller/email.controller.js
@@ -14,7 +14,7 @@ var mailOptions = {
   from: 'yashasvidixit6@gmail.com',
   to: email,
   subject: 'Verification mail Tenders',
-  html: "<h1>Welcome to Tenders app</h1><p>you have successfully register to our app , your login credentials are attached below</p><h2>Email : "+email+"</h2><h2>Password = "+password+"</h2><h1>Click on the link below to redirect</h1>http://localhost:3000/verifyuser/"+email
+  html: "<h1>Welcome to Tenders app</h1><p>you have successfully register to our app , your login credentials are attached below</p><h2>Email : "+email+"</h2><h2>Password = "+password+"</h2><h1>Click on the link below to redirect</h1>https://backend-54l2.onrender.com/verifyuser/"+email
 };
 
 transporter.sendMail(mailOptions, function(error, info){
